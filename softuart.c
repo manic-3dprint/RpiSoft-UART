1e44f25c844f82d726ffd91fc4d8e15313c42bab
manic-3dprint <mainc.3dprint@gmail.com>
2017-09-24 21:25:35 +0800

    removed redundant code

--- softuart.c
+++ softuart.c
@@ -27,29 +27,19 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include <linux/hrtimer.h>
 #include <linux/sched.h>
 #include <linux/string.h>
-#include <linux/slab.h>
 #include <linux/gpio.h>
-#include <asm/io.h>
 
 
-static int BAUDRATE = 4800;
+static int BAUDRATE = 9600;
+module_param(BAUDRATE, int, 0);
+MODULE_PARM_DESC(BAUDRATE, " Baudrate value [default = 4800] (min=1200, max=19200)");
 static int GPIO_TX = 20;
+module_param(GPIO_TX, int, 0);
+MODULE_PARM_DESC(GPIO_TX, " GPIO TX pin [default = 20]");
 static int GPIO_RX = 21;
+module_param(GPIO_RX, int, 0);
+MODULE_PARM_DESC(GPIO_RX, " GPIO RX pin [default = 21]");
 
-/* comment this out for building for a RPi 1 */
-/*
-#define RASPBERRY_PI2_OR_PI3
-
-
-#define IO_ADDRESS(x)  (((x) & 0x00ffffff) + (((x) >> 4) & 0x0f000000) + 0xf0000000)
-#define __io_address(n)  IOMEM(IO_ADDRESS(n))
-#ifdef RASPBERRY_PI2_OR_PI3
-#define BCM2708_PERI_BASE 0x3F000000
-#else
-#define BCM2708_PERI_BASE 0x20000000
-#endif
-#define GPIO_BASE  (BCM2708_PERI_BASE + 0x200000) // GPIO controller 
- */
 
 #define TX_BUFFER_SIZE 256
 #define RX_BUFFER_SIZE 256
@@ -61,63 +51,27 @@ unsigned char RX_DATA = 0;
 unsigned char TX_BUFFER[TX_BUFFER_SIZE + 1];
 unsigned char RX_BUFFER[RX_BUFFER_SIZE + 1];
 bool LOOPBACK = 0;
-/*
-struct GPIO_REGISTERS {
-    uint32_t GPFSEL[6];
-    uint32_t Reserved1;
-    uint32_t GPSET[2];
-    uint32_t Reserved2;
-    uint32_t GPCLR[2];
-    uint32_t Reserved3;
-    uint32_t GPLEV[2];
-} *pGPIO_REGISTER;
- */
 
-//--------------------------------------------------------------------------------------
-
-/*
-static void GPIOFunction(int gpio, int function) {
-    pGPIO_REGISTER->GPFSEL[gpio / 10] = (pGPIO_REGISTER->GPFSEL[gpio / 10] & ~(0b111 << ((gpio % 10) * 3))) | ((function << ((gpio % 10) * 3)) & (0b111 << ((gpio % 10) * 3)));
-}
-
-//--------------------------------------------------------------------------------------
-
-static void GPIOOutputValueSet(int gpio, bool value) {
-    if (value)
-        pGPIO_REGISTER->GPSET[gpio / 32] = (1 << (gpio % 32));
-    else
-        pGPIO_REGISTER->GPCLR[gpio / 32] = (1 << (gpio % 32));
-}
-
-//--------------------------------------------------------------------------------------
-
-static bool GPIOInputValueGet(int gpio) {
-    if (gpio > 31)
-        return (pGPIO_REGISTER->GPLEV[gpio / 32] & (0x00000001 << (gpio - 32))) >> (gpio - 32);
-    else
-        return (pGPIO_REGISTER->GPLEV[gpio / 32] & (0x00000001 << gpio)) >> gpio;
-}
-
-//--------------------------------------------------------------------------------------
- */
 static enum hrtimer_restart FunctionTimerTX(struct hrtimer *t) {
     static int bit = -1;
 
-    if (strlen(TX_BUFFER) > 0 && GPIO_TX) //Data ready to send
+    //printk(KERN_INFO "FunctionTimerTX\n");
+    if (strlen(TX_BUFFER) > 0) //Data ready to send
     {
-        if (bit == -1) {//Start bit 
-            //GPIOOutputValueSet(GPIO_TX, (0 & bit++));
+        //printk(KERN_INFO "FunctionTimerTX sending\n");
+        if (bit == -1) { //Start bit 
             gpio_set_value(GPIO_TX, (0 & bit++));
-        } else if (bit >= 0 && bit <= 7) //Data bits
-        {
+            //printk(KERN_INFO "FunctionTimerTX sending start bit\n");
+        } else if (bit >= 0 && bit <= 7) {//Data bits        
             //GPIOOutputValueSet(GPIO_TX, ((TX_BUFFER[strlen(TX_BUFFER) - 1] & (1 << bit)) >> bit));
             gpio_set_value(GPIO_TX, ((TX_BUFFER[strlen(TX_BUFFER) - 1] & (1 << bit)) >> bit));
             bit++;
+            //printk(KERN_INFO "FunctionTimerTX sending data bit\n");
         } else if (bit == 8) { //Stop bit   
-            //GPIOOutputValueSet(GPIO_TX, 1);
             gpio_set_value(GPIO_TX, 1);
             TX_BUFFER[strlen(TX_BUFFER) - 1] = '\0';
             bit = -1;
+            //printk(KERN_INFO "FunctionTimerTX sending stop bit\n");
         }
     }
 
@@ -130,30 +84,25 @@ static enum hrtimer_restart FunctionTimerTX(struct hrtimer *t) {
 
 static enum hrtimer_restart FunctionTimerRX(struct hrtimer *t) {
     static int bit = -1;
-    if (GPIO_RX) {
-        if (gpio_get_value(GPIO_RX) == 0 && bit == -1) //Start bit received
-            bit++;
-        else if (bit >= 0 && bit < 8) //Data bits
-        {
-            if (gpio_get_value(GPIO_RX) == 0)
-                RX_DATA &= 0b01111111;
-            else
-                RX_DATA |= ~0b01111111;
 
-            if (bit != 7)
-                RX_DATA >>= 1;
+    if (gpio_get_value(GPIO_RX) == 0 && bit == -1) //Start bit received
+        bit++;
+    else if (bit >= 0 && bit < 8) { //Data bits    
+        if (gpio_get_value(GPIO_RX) == 0)
+            RX_DATA &= 0b01111111;
+        else
+            RX_DATA |= ~0b01111111;
 
-            bit++;
-        } else if (bit == 8) //Stop bit
-        {
-            bit = -1;
-
-            RX_BUFFER[strlen(RX_BUFFER)] = RX_DATA;
-
-            if (strlen(RX_BUFFER) == RX_BUFFER_SIZE + 1)
-                memset(RX_BUFFER, '\0', RX_BUFFER_SIZE + 1);
-        }
+        if (bit != 7)
+            RX_DATA >>= 1;
+        bit++;
+    } else if (bit == 8) { //Stop bit    
+        bit = -1;
+        RX_BUFFER[strlen(RX_BUFFER)] = RX_DATA;
+        if (strlen(RX_BUFFER) == RX_BUFFER_SIZE + 1)
+            memset(RX_BUFFER, '\0', RX_BUFFER_SIZE + 1);
     }
+
     hrtimer_forward_now(&hrtimer_rx, ktime_set(0, (1000000 / BAUDRATE)*1000));
 
     return HRTIMER_RESTART;
@@ -191,7 +140,7 @@ static ssize_t get_gpio_tx_callback(struct device* dev, struct device_attribute*
 //--------------------------------------------------------------------------------------
 
 static ssize_t set_gpio_rx_callback(struct device* dev, struct device_attribute* attr, const char* buf, size_t count) {
-    int gpio,prev_gpio;
+    int gpio, prev_gpio;
 
     if (kstrtoint(buf, 10, &gpio) < 0)
         return -EINVAL;
@@ -206,7 +155,7 @@ static ssize_t set_gpio_rx_callback(struct device* dev, struct device_attribute*
     gpio_direction_input(gpio);
     prev_gpio = GPIO_RX;
     GPIO_RX = gpio;
-    gpio_free(prev_gpio);   
+    gpio_free(prev_gpio);
     return count;
 }
 
@@ -303,12 +252,13 @@ static struct device *pDEVICE;
 static int __init ModuleInit(void) {
     int result;
 
-    //pGPIO_REGISTER = (struct GPIO_REGISTERS *) __io_address(GPIO_BASE);
-    //GPIOFunction(GPIO_TX, 0b001); //GPIO as output
-    // GPIOFunction(GPIO_RX, 0b000); //GPIO as input
-    gpio_request(GPIO_TX, "softuart gpio tx");
+
+    if (gpio_request(GPIO_TX, "softuart gpio tx")) return -EAGAIN;
     gpio_direction_output(GPIO_TX, 1);
-    gpio_request(GPIO_RX, "softuart gpio rx");
+    if (gpio_request(GPIO_RX, "softuart gpio rx")) {
+        gpio_free(GPIO_TX);
+        return -EAGAIN;
+    }
     gpio_direction_input(GPIO_RX);
 
     pDEVICE_CLASS = class_create(THIS_MODULE, "soft_uart");
@@ -364,12 +314,7 @@ static void __exit ModuleExit(void) {
 module_init(ModuleInit);
 module_exit(ModuleExit);
 
-module_param(BAUDRATE, int, 0);
-MODULE_PARM_DESC(BAUDRATE, " Baudrate value [default = 4800] (min=1200, max=19200)");
-module_param(GPIO_TX, int, 0);
-MODULE_PARM_DESC(GPIO_TX, " GPIO TX pin [default = 20]");
-module_param(GPIO_RX, int, 0);
-MODULE_PARM_DESC(GPIO_RX, " GPIO RX pin [default = 21]");
+
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Leonardo Ciocari");
 MODULE_DESCRIPTION("Raspberry Pi / BCM2835 Soft-UART Driver");
